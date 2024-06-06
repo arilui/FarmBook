31340892104e8d9f79cb1523334b33c369b3da8c
MariButarov <166068793+MariButarov@users.noreply.github.com>
2024-06-06 15:45:18 -0700

    Merge branch 'main' of https://github.com/arilui/FarmBook

--- public/sellerpage.js
+++ public/sellerpage.js
@@ -1,1 +1,13 @@
-// TODO: add sellerpage javascript
+document.addEventListener("DOMContentLoaded", function() {
+    // Function to redirect to index page when logo button is clicked
+    function redirectToIndexPage() {
+        console.log("Redirecting to index page...");
+        window.location.href = "index.html";
+    }
+    
+    
+    
+     // Attach event listeners
+     document.querySelector(".logo-button").addEventListener("click", redirectToIndexPage);
+    
+    });
